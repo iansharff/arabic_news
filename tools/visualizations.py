17b533cddf4770cdacad9d706b61a02ca3f6285c
iansharff <ips11@georgetown.edu>
2021-09-07 00:24:15 -0400

    Create visualization.py

---
+++ tools/visualizations.py
@@ -0,0 +1,14 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.metrics import confusion_matrix
+
+def plot_confusion_matrices():
+    pass
+
+def plot_clusters():
+    pass
+
+def word_map():
+    pass
