6f42e647e025ca295dac27ca280fb74173ccabf5
iansharff <ips11@georgetown.edu>
2021-09-13 15:25:20 -0400

    Defined partial fit for NB
    Training NB model on batches

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
 import tensorflow as tf
 from tensorflow.keras.preprocessing import text, text_dataset_from_directory
 
@@ -11,20 +14,39 @@ from sklearn.model_selection import GridSearchCV
 from tools import misc
 from tools import visualizations as vis
 
-BATCH_SIZE = 32
-VALIDATION_SPLIT = 0.2
-SEED = 123
-
 def main():
-    pass
+    train_ds = get_dataset('raw_data/sanad_full_copy', 'training')
+    val_ds = get_dataset('raw_data/sanad_full_copy', 'validation').unbatch()
 
-def get_dataset(path):
+    nb_model = MultinomialNB()
+    count_vectorizer = CountVectorizer(
+        encoding = 'utf-8',
+        strip_accents= 'unicode',
+        lowercase=False,
+        token_pattern = misc.PATTERN
+    )
+    for i, (text_batch, label_batch) in enumerate(train_ds):
+        if i == 0:
+            text_batch_cv = count_vectorizer.fit_transform(text_batch.numpy())
+            nb_model.partial_fit(text_batch_cv, label_batch.numpy(), classes=np.arange(7))
+
+        else:
+            text_batch_cv = count_vectorizer.transform(text_batch.numpy())
+            nb_model.partial_fit(text_batch_cv, label_batch.numpy())
+        print(f"Batch {i+1}/{len(train_ds)} fitted")
+        
+
+def get_dataset(path, subset):
     return text_dataset_from_directory(
         directory=path,
         labels='inferred',
-        batch_size=BATCH_SIZE,
-        validation_split=VALIDATION_SPLIT
+        label_mode='int',
+        batch_size=misc.BATCH_SIZE,
+        subset=subset,
+        validation_split=misc.VALIDATION_SPLIT,
+        seed=misc.SEED
     )
+
 
 def model_nb():
     pass
