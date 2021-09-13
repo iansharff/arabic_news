bb050412292543ccaf92c5f96e414242326eb3d5
iansharff <ips11@georgetown.edu>
2021-09-13 17:45:23 -0400

    Pickle full corpus model
    Lines added to models.py to save the full model along with validation
    data/labels and the fitted count_vectorizer

--- models.py
+++ models.py
@@ -9,14 +9,13 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import SGDClassifier, Perceptron
 
 from sklearn.feature_selection import RFECV
-from sklearn.model_selection import GridSearchCV
 
 from tools import misc
 from tools import visualizations as vis
 
 def main():
     train_ds = get_dataset('raw_data/sanad_full_copy', 'training')
-    val_ds = get_dataset('raw_data/sanad_full_copy', 'validation').unbatch()
+    val_ds = get_dataset('raw_data/sanad_full_copy', 'validation', batch_size=39034)
 
     nb_model = MultinomialNB()
     count_vectorizer = CountVectorizer(
@@ -34,9 +33,19 @@ def main():
             text_batch_cv = count_vectorizer.transform(text_batch.numpy())
             nb_model.partial_fit(text_batch_cv, label_batch.numpy())
         print(f"Batch {i+1}/{len(train_ds)} fitted")
+    
+    val_texts, val_labels = next(iter(val_ds))
+    val_texts = val_texts.numpy()
+    val_labels = val_labels.numpy()
+    val_texts_cv = count_vectorizer.transform(val_texts)
+
+    misc.save(nb_model, 'outputs/models/nb_full_train.pkl')
+    misc.save(val_texts_cv, 'outputs/models/val_texts_cv.pkl')
+    misc.save(val_labels, 'outputs/models/val_labels.pkl')
+    misc.save(count_vectorizer, 'outputs/models/count_vectorizer.pkl')
         
 
-def get_dataset(path, subset):
+def get_dataset(path, subset, batch_size=misc.BATCH_SIZE):
     return text_dataset_from_directory(
         directory=path,
         labels='inferred',
