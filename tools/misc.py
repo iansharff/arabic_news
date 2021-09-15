4a3068cea668ae258b7d3f60e6c85632cb645d72
iansharff <ips11@georgetown.edu>
2021-09-15 17:37:01 -0400

    Add comments

--- tools/misc.py
+++ tools/misc.py
@@ -5,8 +5,7 @@ import requests
 import nltk
 from pprint import pprint
 
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.model_selection import GridSearchCV
 
 PATTERN = r'[\u0621-\u064A]+'
 STOPWORDS_URL = 'https://raw.githubusercontent.com/mohataher/arabic-stop-words/master/list.txt'
@@ -39,6 +38,7 @@ def load(filepath):
     return obj
 
 def get_stopwords(url = STOPWORDS_URL):
+    """Obtain stopwords from either the source on GitHub or the NLTK default stopword list for Arabic"""
     r = requests.get(STOPWORDS_URL)
     stopwords = []
     if r.status_code:
@@ -48,11 +48,13 @@ def get_stopwords(url = STOPWORDS_URL):
     return stopwords
 
 def get_absolute_paths(directory):
+    """Get a list of absolute filepaths within a directory recursively"""
     for dirpath, _, filenames in os.walk(directory):
         for filename in filenames:
             yield os.path.abspath(os.path.join(dirpath, filename))
 
 def run_grid_search(X_train, y_train, pipeline, param_grid):
+    """Run a grid search, provided data, labels, a pipeline with an estimator, and a parameter grid"""
     grid_search = GridSearchCV(pipeline, param_grid, n_jobs=-1, verbose=1)
     
     print("Grid Searching...")
